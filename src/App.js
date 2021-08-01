d7bfda116f8e9a2e7b391ee6483361e8faf449e6
Virginia Rabello <virginia.rabello2107@gmail.com>
2021-07-31 21:46:55 -0500

    About section typewriter effect feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import About from './Components/About';
+document.body.classList.add('hero');
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <About></About>
   );
 }
-
 export default App;
